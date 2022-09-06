fc366e292f96819fa72ab7f930cee9da87924201
EvanKim <youngchan.kim@edu.sait.ca>
2022-09-06 14:23:16 -0600

    added checklist api

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var app = express();
 
+let comments = [];
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 // set the view engine to ejs
 app.set("view engine", "ejs");
 
@@ -8,7 +13,21 @@ app.set("view engine", "ejs");
 
 // index page
 app.get("/", function (req, res) {
-  res.render("index");
+  // res.render("index", { num: 30 });
+  res.render("index", { comments: comments });
+});
+
+app.get("/create", (req, res) => {
+  console.log(req.query);
+  res.send("get hi");
+});
+
+app.post("/create", (req, res) => {
+  console.log(req.body);
+  const { content } = req.body;
+  comments.push(content);
+  console.log(comments);
+  res.redirect("/");
 });
 
 app.listen(8080);
